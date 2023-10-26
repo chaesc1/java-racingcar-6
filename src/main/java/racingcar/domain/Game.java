aa1dc35676c3e78ca9bd4bd1f298f5426e255c85
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-10-26 16:36:11 +0900

    feat : 매 시도 마다 진행상황 표시

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,2 +1,25 @@
-package racingcar.domain;public class Game {
+package racingcar.domain;
+
+import java.util.List;
+
+public class Game {
+    private final int trial;
+    private final List<Car> cars;
+    private int trialSum = 0;
+    public Game(List<Car> cars, int trial) {
+        this.trial = trial;
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void addTrialnum() {
+        this.trialSum += 1;
+    }
+
+    public boolean checkReachTrialToTrialSum() {
+        return this.trialSum == this.trial;
+    }
 }

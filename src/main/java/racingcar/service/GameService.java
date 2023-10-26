7eb99917c0bc61d95156b4ebcba53d5dae70ff08
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-10-26 18:01:00 +0900

    feat : 랜덤 생성 메서드 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,9 +1,11 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
 import racingcar.domain.Game;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
     private static final int START = 0;
@@ -21,5 +23,10 @@ public class GameService {
             carList.add(new Car(carNames.get(i)));
         }
         return carList;
+    }
+
+    //랜덤 숫자 받기
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(START, END);
     }
 }

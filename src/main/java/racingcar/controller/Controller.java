64acd65b6e315f5d6c576840c43d140d13648f54
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-10-26 18:27:20 +0900

    feat : 게임 종료시 우승자 출력/게임 진행상황 출력

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,18 +1,37 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.Car;
+import racingcar.service.GameService;
 import racingcar.utils.Parser;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Controller {
+    private GameService gameService;
+    private OutputView outputView = new OutputView();
     private Parser parser = new Parser();
+
     public void run() {
         //Input 받아야해
         InputView.requsetCarNameMessage();
-        List<String> carnames = parser.parsingCarName(Console.readLine());
+        List<String> carnames = parser.parseCarName(Console.readLine());
         InputView.requestTrialNumMessage();
+        List<Car> cars = getCars(carnames);
         int trial = parser.parsingNumberOfTrial(Console.readLine());
+        gameService = new GameService(trial, cars);
+        List<Car> winners = gameService.play();
+        outputView.printResults(cars);
+        outputView.printWinners(winners);
+    }
 
+    private List<Car> getCars(List<String> carNames) {
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            carList.add(new Car(carNames.get(i)));
+        }
+        return carList;
     }
 }
--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -13,29 +13,44 @@ public class GameService {
     private static final int MOVE_FORWARD_CONDITION = 4;
     private Game game;
 
-    public GameService(int trial, List<String> carNames) {
-        this.game = new Game(trial, getCars(carNames));
+    public GameService(int trial, List<Car> cars) {
+        this.game = new Game(trial, cars);
     }
 
-    private List<Car> getCars(List<String> carNames) {
-        List<Car> carList = new ArrayList<>();
-        for (int i = 0; i < carNames.size(); i++) {
-            carList.add(new Car(carNames.get(i)));
+    public List<Car> play() {
+        while (!game.checkReachTrialToTrialSum()) {
+            moveForardToRandomNumber();
+            game.addTrialnum();
         }
-        return carList;
+        return getWinners();
     }
 
+    private List<Car> getWinners() {
+        int max = -1;
+        List<Car> winners = new ArrayList<>();
+        for (Car car : game.getCars()) {
+            if (max == car.getPosition()) {
+                winners.add(car);
+            }
+            if (max < car.getPosition()) {
+                max = car.getPosition();
+                winners = new ArrayList<>();
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private void moveForardToRandomNumber() {
+        for (Car car : game.getCars()) {
+            if (MOVE_FORWARD_CONDITION <= getRandomNumber()) {
+                car.moveForward(1);
+            }
+        }
+    }
     //랜덤 숫자 받기
     private int getRandomNumber() {
         return Randoms.pickNumberInRange(START, END);
     }
 
-    public void play() {
-        for (Car car : game.getCars()) {
-            if(MOVE_FORWARD_CONDITION <= getRandomNumber()){
-                car.moveForward(1); //자동차를 1칸 올리고
-            }
-            game.addTrialnum(); //게임 시도 횟수를 1회 올린다.
-        }
-    }
 }
